b7b3e63316edff130a8d0d54c0c329b3a8efb928
Kevin Holder <kholder@gmail.com>
2020-04-01 13:50:46 -0500

    initial code commit

---
+++ BethanysPieShopMobile/PieDetailActivity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace BethanysPieShopMobile
+{
+    [Activity(Label = "PieDetailActivity", MainLauncher=true)]
+    public class PieDetailActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.pie_detail);
+            // Create your application here
+        }
+    }
+}
