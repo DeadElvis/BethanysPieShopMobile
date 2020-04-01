1c5f92aa8f37e55b6761c476527cc21b6f752462
Kevin Holder <kholder@gmail.com>
2020-04-01 15:59:53 -0500

    commit day's activity

--- BethanysPieShopMobile/PieDetailActivity.cs
+++ BethanysPieShopMobile/PieDetailActivity.cs
@@ -2,24 +2,61 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
 using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Support.V7.Widget;
 using Android.Views;
 using Android.Widget;
+using BethanysPieShopMobile.Core.Models;
+using BethanysPieShopMobile.Core.Repository;
+using BethanysPieShopMobile.Utility;
 
 namespace BethanysPieShopMobile
 {
     [Activity(Label = "PieDetailActivity", MainLauncher=true)]
     public class PieDetailActivity : Activity
     {
+        private PieRepository _pieRepository;
+        private Pie _selectedPie;
+        private ImageView _pieImageview;
+        private TextView _pieNameTextView;
+        private TextView _shortDescriptionTextView;
+        private TextView _descriptionTextView;
+        private TextView _priceTextView;
+        private TextView _amountEditText;
+        private Button _addToCartButton;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.pie_detail);
-            // Create your application here
+            _pieRepository = new PieRepository();
+            _selectedPie = _pieRepository.GetPieById(1);
+            FindViews();
+            BindData();
+        }
+
+        private void BindData()
+        {
+            _pieNameTextView.Text = _selectedPie.Name;
+            _shortDescriptionTextView.Text = _selectedPie.ShortDescription;
+            _descriptionTextView.Text = _selectedPie.LongDescription;
+            _priceTextView.Text = "Price: " + _selectedPie.Price.ToString();
+            var imageBitmap = ImageHelper.GetImageBitmapFromUrl(_selectedPie.ImageThumbnailUrl);
+            _pieImageview.SetImageBitmap(imageBitmap);
+
+        }
+
+        private void FindViews()
+        {
+            _pieImageview = FindViewById<ImageView>(Resource.Id.pieImageView);
+            _pieNameTextView = FindViewById<TextView>(Resource.Id.pieNameTextView);
+            _shortDescriptionTextView = FindViewById<TextView>(Resource.Id.shortDescriptionTextView);
+            _descriptionTextView = FindViewById<TextView>(Resource.Id.descriptionTextView);
+            _priceTextView = FindViewById<TextView>(Resource.Id.priceTextView);
+            _amountEditText = FindViewById<TextView>(Resource.Id.amountEditText);
+            _addToCartButton = FindViewById<Button>(Resource.Id.addToCartButton);
         }
     }
 }
